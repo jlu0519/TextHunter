8d4667cf60284a4ff47fccaddacd8e9ad97cfff7
jlu0519 <lukeyoung0519@gmail.com>
2026-07-20 15:31:53 -0700

    Implemented basic search of user provided text file.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <fstream>
 #include <string>
 
-int main(){
- 
+int main()
+{
+    // Store user-provided file path and search pattern
+    std::string userFile;
+    std::string searchTxt;
+
     std::cout << "Enter a filename to search!\n";
 
-    std::string userFile;
-
     std::getline(std::cin, userFile);
+
+    std::cout << "Enter a Text pattern to search!\n";
+
+    std::getline(std::cin, searchTxt);
 
     // Open file using the stream constructor
     std::ifstream file(userFile);
@@ -20,14 +26,17 @@ int main(){
     }
 
     std::string line;
-    // Condition checks if a line was successfully read
+
+    // Condition checks if a line was successfully read.
+    // Only saving one line at a time to handle large files the same as smaller files.
     while(std::getline(file,line)) {
-        // Process your line here
-        std::cout << line << "\n";
+        // Returns line if Searched text is found
+        if(line.find(searchTxt) != std::string::npos)
+        {
+            std::cout << line << "\n";
+        }
     }
 
-    // Closes automatically when "file" goes out of scope
-    // but you can call close() explicitly if needed.
     file.close();
     return 0;
 }
