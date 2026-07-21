6e24ca60ef407f470fa8e36718529b98b93d2228
Luke Young <lukeyoung0519@gmail.com>
2026-07-21 06:50:33 -0700

    Added basic argument validation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 
 int main(int argc, char* argv[])
 {
+    // Verify that both the file path and search pattern were provided
+    if(argc <= 2)
+    {
+        std::cerr << "Too few arguments provided!" << std::endl;
+        return 1;
+    }
+
     // Store user-provided file path and search pattern
     std::string userFile = argv[1]; 
     std::string searchTxt = argv[2];
@@ -15,7 +22,7 @@ int main(int argc, char* argv[])
     // Checking if file can be opened successfully 
     if(!file.is_open()) {
         std::cerr << "Error opening file!" << std::endl;
-        return 1;
+        return 2;
     }
 
     std::string line;
@@ -24,7 +31,7 @@ int main(int argc, char* argv[])
     // Condition checks if a line was successfully read.
     // Only saving one line at a time to handle large files the same as smaller files.
     while(std::getline(file,line)) {
-        // Outputs line if Searched text is found
+        // Outputs line if searched text is found
         if(line.find(searchTxt) != std::string::npos)
         {
             std::cout <<userFile << ":" << lineNumber << ":" << line << "\n";
@@ -33,7 +40,6 @@ int main(int argc, char* argv[])
         lineNumber++;
     }
 
-    file.close();
     return 0;
 }
 
